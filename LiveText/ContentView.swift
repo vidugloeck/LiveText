25cc0ddb9a03b6833d2e0098ed49d9d66585fa87
Vidu Glöck <git@vidugloeck.com>
2021-06-09 18:21:17 +0200

    Initial Commit

---
+++ LiveText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveText
+//
+//  Created by Vidu Glöck on 09.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveText/LiveTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTextApp.swift
+//  LiveText
+//
+//  Created by Vidu Glöck on 09.06.21.
+//
+
+import SwiftUI
+
+@main
+struct LiveTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
